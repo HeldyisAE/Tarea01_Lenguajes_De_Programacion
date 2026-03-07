320485b0a50d764e49a3a204648d3c359228b972
HeldyisAE <heaguero@estudiantec.cr>
2026-03-07 00:50:13 -0600

    Agrega un tag del jugador activo en la partida
    De momento solo es una simulación

---
+++ client/src/components/PlayerTag.jsx
@@ -0,0 +1,11 @@
+import "./PlayerTag.css"
+
+function PlayerTag() {
+    return(
+        <div className="player-tag">
+            <h2>HeldyisAE</h2>
+        </div>
+    );
+}
+
+export default PlayerTag;
