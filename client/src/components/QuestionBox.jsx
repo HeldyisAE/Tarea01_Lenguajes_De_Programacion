d8721fef7126b997aefb1639945ca2dfcbcd8183
HeldyisAE <heaguero@estudiantec.cr>
2026-03-06 23:43:08 -0600

    Se agrega el componente QuestionBox para poder mostrar las preguntas del juego

---
+++ client/src/components/QuestionBox.jsx
@@ -0,0 +1,11 @@
+import "./QuestionBox.css"
+
+function QuestionBox() {
+    return (
+        <div className="question-box">
+            <span>¿Quién fue el primer presidente de Estados Unidos?</span>
+        </div>
+    );
+}
+
+export default QuestionBox;
