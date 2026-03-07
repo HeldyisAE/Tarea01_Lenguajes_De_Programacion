5d9675634de4b587d4fa73c70d1140143522549e
HeldyisAE <heaguero@estudiantec.cr>
2026-03-07 00:50:13 -0600

    Agrega un tag del jugador activo en la partida
    De momento solo es una simulación

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { useLocation } from 'react-router-dom'
 import './Header.css'
 import ResetButton from './ResetButton';
+import PlayerTag from './PlayerTag';
+
 
 export default function Header() {
+    const location = useLocation()
+    const isGame = location.pathname === "/game"
+
     return (
         <header className="header">
             <div className="header-left">
@@ -11,7 +17,7 @@ export default function Header() {
                 <h2>Pregunta2</h2>
             </div>
             <div className="header-right">
-
+                {isGame && <PlayerTag />}
             </div>
         </header>
     );
