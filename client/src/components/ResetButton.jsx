40cf5fe3297a066e969dfb46e225dc01c71fb029
HeldyisAE <heaguero@estudiantec.cr>
2026-03-07 21:50:49 -0600

    Implementa funcionamiento del botón reset

--- client/src/components/ResetButton.jsx
+++ client/src/components/ResetButton.jsx
@@ -1,9 +1,16 @@
 import { PiSpinnerBall } from "react-icons/pi";
+import { useNavigate } from "react-router-dom";
 import './ResetButton.css'
 
 export default function ResetButton() {
+    const navigate = useNavigate();
+
+    const handleReset = () => {
+        navigate("/");
+    };
+
   return(
-        <button className="reset-button">
+        <button className="reset-button" onClick={handleReset}>
             <div className="reset-button-logo">   
                 <PiSpinnerBall size={48} />
             </div> 
