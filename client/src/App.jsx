3e0ed0f30d120724fd2a78ce27aca50e6b4d033a
HeldyisAE <heaguero@estudiantec.cr>
2026-03-08 15:04:04 -0600

    Agrega archivo de audio para música de fondo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,30 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
 import "./App.css";
+import bgMusicFile from "./assets/Pregunta2__Soundtrack.mp3";
 
 /**
  * Componente principal de la app
  * Define la navegación entre las páginas
+ * Además reproduce la música de fondo
  * 
  * @returns {JSX.Element} Elemento JSX que representa el flujo de navegación
  */
 
 function App() {
   return (
+    <>
+      {/* Música de fondo, siempre activa */}
+      <audio autoPlay loop>
+        <source src={bgMusicFile} type="audio/mp3" />
+        Tu navegador no soporta audio
+      </audio>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
       </Routes>
+    </>
   );
 }
 
